a4dcdcf4fb585030be65c1601fc535a4557a5de8
andersonmarqueszero <113368673+andersonmarqueszero@users.noreply.github.com>
2023-01-02 19:25:16 -0300

    alert + console commands + variables + data types

---
+++ script-tags/main.js
@@ -0,0 +1,52 @@
+/*
+alert('Hello World');
+
+-----------------------------------------------------------------------
+
+console.log('Hello world');
+console.error('This is an error');
+console.warn('This is a warning');
+
+-----------------------------------------------------------------------
+
+// variables: var (outdated), let, const //
+// with "let" it's possible to change the variable, like displayed below //
+
+let age = 30;
+age = 31;
+
+console.log(age);
+
+// with "const" it's not possible to change the variable, the code below displays an error//
+
+const age = 30;
+age = 31;
+
+console.log(age);
+
+// the tip is to always use "const", unless you know you want to reassign the variable, for example //
+
+let score;
+
+score = 10;
+
+console.log(score);
+
+// if you try to assign "const" in the example above, you will get an error, since you're required to input a value to your variable //
+
+-----------------------------------------------------------------------
+
+// These are some examples of data types: String, Numbers, Boolean, null, undefined, Symbol //
+
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+// with the undefined variable, you can define the undefined, like it's displayed in y, or you can use "let" to achieve the same effect, like it's shown in z //
+
+console.log(typeof z);
+*/
