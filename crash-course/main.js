8d666b3431475aed7a613a609d1760a4887538c9
andersonmarqueszero <113368673+andersonmarqueszero@users.noreply.github.com>
2023-01-11 23:22:47 -0300

    Constructive Functions e Prototypes

--- crash-course/main.js
+++ crash-course/main.js
@@ -452,3 +452,47 @@ todos.forEach((todo) => console.log(todo));
 
 --------------------------------------------------------------------------
 */
+//Constructive Functions & Prototypes
+
+//Constructor Function
+
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = dob;
+}
+
+// Instantiate object
+
+const person1 = new Person('John', 'Doe', '4-3-1980');
+const person2 = new Person('Mary', 'Smith', '3-6-1970');
+
+console.log(person1);
+console.log(person2.firstName);
+
+//It's also possible to use the "dates" as a "date object", by doing the following
+
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+}
+
+console.log(person2.dob.getFullYear());
+
+//You can also add methods (functions) to this same Person object
+
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+    this.getBirthYear = function() {
+        return this.dob.getFullYear();
+    }
+    this.getFullName = function() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+console.log(person1.getBirthYear());
+console.log(person1.getFullName());
