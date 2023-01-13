74d5a2cb515efa34955b5e004ae1f208e38e614a
andersonmarqueszero <113368673+andersonmarqueszero@users.noreply.github.com>
2023-01-12 21:09:57 -0300

    3S6 Classes

--- crash-course/main.js
+++ crash-course/main.js
@@ -494,6 +494,9 @@ function Person(firstName, lastName, dob) {
     }
 }
 
+const person1 = new Person('John', 'Doe', '4-3-1980');
+const person2 = new Person('Mary', 'Smith', '3-6-1970');
+
 console.log(person1.getBirthYear());
 console.log(person1.getFullName());
 
@@ -502,13 +505,7 @@ console.log(person1.getFullName());
 function Person(firstName, lastName, dob) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.dob = new Date(dob);
-    this.getBirthYear = function() {
-        
-    }
-    this.getFullName = function() {
-        
-    }
+    this.dob = new Date(dob);    
 }
 
 Person.prototype.getBirthYear = function() {
@@ -524,3 +521,43 @@ console.log(person1);
 
 --------------------------------------------------------------------------
 */
+
+//3S6 Classes
+
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);    
+}
+
+Person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear();
+}
+
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+}
+
+//Class
+class Person {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = new Date(dob);
+    }
+
+    getBirthYear() {
+        return this.dob.getFullYear();
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+//Instantiate object
+const person1 = new Person('John', 'Doe', '4-3-1980');
+const person2 = new Person('Mary', 'Smith', '3-6-1970');
+
+console.log(person2.getFullName());
+console.log(person1);
