86917f9240d8c9ce96b6b54a3af3ed2ea715deb7
andersonmarqueszero <113368673+andersonmarqueszero@users.noreply.github.com>
2023-01-15 22:08:32 -0300

    form script

--- crash-course/main.js
+++ crash-course/main.js
@@ -611,3 +611,33 @@ btn.addEventListener('click', (e) => {
 
 --------------------------------------------------------------------------
 */
+//Form Script
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('.msg');
+const msg = document.querySelector('#my-form');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        // Clear fields
+
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
